28c35bd1df5619a665f1098b26ac7427fdea2c77
M-buria <98148858+M-buria@users.noreply.github.com>
2022-11-26 17:33:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict"
+// урок №2
+console.log('Я вчу JS');
+
+// console.log('вчимо');console.log('js');
+
+
+// урок №3
+//1)
+let userEyeColor;
+//2)
+let user;
+let userName = 'Вася';
+user = userName;
+console.log(user);
+
+/*
+const BLOCK_SIZE = 14 + 50;
+console.log(BLOCK_SIZE);*/
+
